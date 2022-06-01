b22d8b54e715ae2a5ce7d3364df4f2cca2c336c7
Lauren McCarthy <laurenleemccarthy@gmail.com>
2022-05-31 18:03:58 -0700

    player

---
+++ player/main.js
@@ -0,0 +1,38 @@
+let button = document.querySelector('#button');
+let audio = document.querySelector('#audio');
+let video = document.querySelector('#video');
+
+$(window).on('click', restart);
+$(window).on('load', load);
+$('#video').on('ended', load);
+
+
+function load() {
+  $('#instruct').show();
+}
+
+function restart() {
+  console.log('restart');
+  if (audio) {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0
+    }
+    console.log("restart audio")
+  } else if (video) {
+    console.log("restart video")
+    $('#instruct').hide();
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+      $('#video').fadeOut(1000, () => {
+        video.currentTime = 0;
+        $('#video').fadeIn(1000, () => {
+          video.play();
+        })
+      });
+    }
+  }
+}
